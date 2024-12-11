62ab1216d761f55325adc85cda02513ae7ac48fc
1948771 <1948771@01-18129.pusd.dom>
2024-12-11 13:53:56 -0800

    r

---
+++ AdventOfCode/src/Runner.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public String extactMult(String line) {
+			if(line.indexOf("mul") == -1) {
+				return "";
+			}else {
+				int start = line.indexOf("mul(");
+			}//d
+		}
+		File file =new File("input.txt");
+		
+		try {
+			Scanner scanner = new Scanner(file);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block+
+			
+			e.printStackTrace();
+		}
+	}
+
+}
